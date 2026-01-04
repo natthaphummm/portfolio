02e4f1f6f701f7b03b2a0c1d122932938c19ef4c
Natthaphum Phusong <info.hzelnat@gmail.com>
2026-01-05 01:29:17 +0700

    feat: use tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   test: {
     globals: true,
     environment: "jsdom",
