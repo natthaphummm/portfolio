96c39803064875204c6ab9af5019ca3ab82d6721
Natthaphum Phusong <info.hzelnat@gmail.com>
2026-01-05 09:09:35 +0700

    feat: add github page

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/portfolio/",
   test: {
     globals: true,
     environment: "jsdom",
